d98426dab1c728128bbbd60941f479fdd409eb94
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-23 10:13:07 -0400

    Add intro component and css

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,9 +1,20 @@
 import React from "react"
 
+import MastercraftLogo from "../Assets/images/logo-mastercraft.svg"
+import bookmark from "../Assets/images/icon-bookmark.svg"
+
 function Intro(){
     return(
-        <div className="product-info-inner">
-
+        <div id="intro" className="product-info-inner product-info-inner--intro">
+            <img className="mastercraft-logo" src={MastercraftLogo} alt="Mastercraft Logo" />
+            <h2>Mastercraft Bamboo Monitor Riser</h2>
+            <p>A beautifully crafted monitor stand to reduce neck and eye strain</p>
+            <div className="row-space-between">
+                <button>Back this project</button>
+                <div>
+                    <img className="bookmark" src={bookmark} alt="Bookmark" />
+                </div>
+            </div>
         </div>
     )
 }
--- src/Layouts/ProductInfo.jsx
+++ src/Layouts/ProductInfo.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Intro from "../Components/Intro";
 
+import "../Assets/Styles/ProductInfo/ProductInfo.css"
+
 function ProductInfo(){
     return(
         <section id="product-info">
